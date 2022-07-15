d488927fbfb39cee54db6a7faaf34a2fea3368db
NEEL <neelshah682003@gmail.com>
2022-07-15 17:46:43 +0530

    second commit

--- Pattern40.java
+++ Pattern40.java
@@ -12,10 +12,12 @@
 import java.util.Scanner;
 
 public class Pattern40 {
+    
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         
         System.out.println("Enter Number of Rows: ");
-         Scanner sc = new Scanner(System.in);
+
         int num = sc.nextInt();
 
         for(int i=1;i<=num;i++)
