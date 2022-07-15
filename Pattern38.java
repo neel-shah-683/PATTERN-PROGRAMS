f999ca997540acf26485415d3976d2b7c4ed23d7
NEEL <neelshah682003@gmail.com>
2022-07-15 15:49:22 +0530

    first commit

---
+++ Pattern38.java
@@ -0,0 +1,40 @@
+/*
+    *****
+    ****
+    ***
+    **
+    *
+    **
+    ***
+    ****
+    *****
+ */
+public class Pattern38 {
+    public static void main(String[] args) {
+        
+        int k=0;
+        for(int i=1;i<=9;i++)
+        {
+            if(i<=5)
+            {
+                k++;
+            }
+            else 
+            {
+                k--;
+            }
+            for(int j=1;j<=5;j++)
+            {
+                if(j<=6-k)
+                {
+                    System.out.print("*");
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
